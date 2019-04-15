b5263782f16d4fd729240770006fa1e8e5711a5a
jjkv <jjkernsv@gmail.com>
2019-04-14 21:21:05 -0400

    documentation

--- Prom.java
+++ Prom.java
@@ -3,14 +3,22 @@ import java.lang.reflect.*;
 
 class Micah {
 
-    private static final String askMethod = "askedToPromBy"; 
+    /* teenagers have a surprisingly standardized communication protocol */
+    private static final String askMethod = "askedToPromBy";
+
+    /* micah the extrovert (a whelmed one) */
     public static String mood = "meh";
-    
+
+    /* please be Hannah */
     public static void askToProm(String date_name) {
+
         try {
-            Class<?> asking = Class.forName(date_name);
-            Method cheesy_poster = asking.getMethod(askMethod, Micah.class);
-            cheesy_poster.invoke(null, Micah.class.newInstance());
+            Class<?> asking        = Class.forName(date_name);
+            Method   cheesy_poster = asking.getMethod(askMethod, Micah.class);
+            
+            /* i invoke, CHEESY POSTER (* gasp *)*/
+            cheesy_poster.invoke(null, Micah.class.newInstance());            
+        /* catching the following exceptions individually is mostly just for fun */
         } catch (ClassNotFoundException e) {
             mood = "hannah wya?";
         } catch (NoSuchMethodException e) {
@@ -25,15 +33,30 @@ class Micah {
     }
 }
 
+/* everything is static because women aren't objects */
 class Hannah {
 
     public static String answer = "";
 
+    /* decided to give Micah the win here in accordance with the poster itself */
     public static void askedToPromBy(Micah m) { answer = "yes"; }
 
-    /* public static void askedToPromBy(Chad c) { answer = "YES"; } */
-}
+    /* pandering: 
+    public static void askedToPromBy(Chad c)   { answer = "YES"; } 
+    public static void askedToPromBy(Virgin v) { answer = "ew";  }
+    */
+}   
 
+/* 
+problems: 
+ - `==` instead of `.equals()` is BAD
+ - `Hannah.answer` should be a boolean
+ - askToProm should a Hannah instance
+ - Micah and Hannah should be singleton objects
+ - //Prom? 
+ - poor use of naming idioms
+ - storing mood as string, rather than possibly enum of emotions
+ */
 public class Prom {
 
     public static void main(String [] argv) {
@@ -41,6 +64,7 @@ public class Prom {
         if(Hannah.answer == "yes") {
             Micah.mood = "Happy";
         } else {
+            /* you're done kiddo */
             System.out.println("hannah's answer: "+Hannah.answer);
         }                
         System.out.println("Micah's mood: "+Micah.mood);
